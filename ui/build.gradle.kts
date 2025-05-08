b629a53ef8f8c541c5d82b63c7396596768e9f16
Moritz Lindner <lindner197@gmail.com>
2025-05-08 16:26:12 +0200

    add basic app

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -13,14 +13,14 @@ repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
+    maven("https://raw.githubusercontent.com/apromore/ApromoreCore_SupportLibs/master/mvn-repo/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+
+    implementation(project(":epa"))
 }
 
 compose.desktop {
--- ui/src/main/kotlin/Main.kt
+++ ui/src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -6,20 +6,58 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import moritz.lindner.masterarbeit.epa.ExtendedPrefixAutomata
+import moritz.lindner.masterarbeit.epa.builder.BPI2017ChallengeEventMapper
+import moritz.lindner.masterarbeit.epa.builder.ExtendedPrefixAutomateBuilder
+import moritz.lindner.masterarbeit.epa.visitor.StatisticsVisitor
+import java.io.File
 
 @Composable
-@Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+
+    val scope = rememberCoroutineScope()
+
+    var text by remember { mutableStateOf("Build EPA") }
+    var epa by remember { mutableStateOf<ExtendedPrefixAutomata<Long>?>(null) }
+
+    val file = File("./ui/src/main/resources/eventlogs/BPI Challenge 2017.xes.gz")
+    val mapper = BPI2017ChallengeEventMapper()
+
+    val epaBuilder = ExtendedPrefixAutomateBuilder<Long>()
+        .setFile(file)
+        .setEventLogMapper(mapper)
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+
+        if (epa != null) {
+            BasicText(
+                text = "epa: ${epa?.states?.size} states",
+            )
+            Button(onClick = {
+                scope.launch(Dispatchers.IO) {
+                    val statistics = StatisticsVisitor<Long>(epa!!)
+                    epa!!.acceptDepthFirst(statistics)
+                    text = "${statistics.report()}"
+                }
+            }) {
+                Text("Create statistics")
+            }
+        }else {
+            Button(onClick = {
+                scope.launch(Dispatchers.IO) {
+                    text = "Start Constructing"
+                    epa = epaBuilder.build()
+                    text = "End Constructing"
+                }
+            }) {
+                Text(text)
+            }
         }
     }
 }
