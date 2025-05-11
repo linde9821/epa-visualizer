726be0efbae74481bd1b0405f4fc5fdce396a139
Moritz Lindner <lindner197@gmail.com>
2025-05-11 15:39:39 +0200

    update epa to support ne visitor capabilities

--- epa/src/main/kotlin/moritz/lindner/masterarbeit/epa/ExtendedPrefixAutomata.kt
+++ epa/src/main/kotlin/moritz/lindner/masterarbeit/epa/ExtendedPrefixAutomata.kt
@@ -6,33 +6,42 @@ import moritz.lindner.masterarbeit.epa.domain.State
 import moritz.lindner.masterarbeit.epa.domain.Transition
 import moritz.lindner.masterarbeit.epa.visitor.AutomataVisitor
 
+
 /**
  * Not thread safe to visit
  */
-data class ExtendedPrefixAutomata<T : Comparable<T>>(
+// TODO: specify collection implementations
+class ExtendedPrefixAutomata<T : Comparable<T>>(
     val states: Set<State>,
     val activities: Set<Activity>,
     val transitions: Set<Transition>,
     private val partitionByState: Map<State, Int>,
     private val sequenceByState: Map<State, Set<Event<T>>>,
 ) {
+
     // for improved visitor speed
-    private val outgoingTransitionsByState = transitions.groupBy { it.start }
+    val outgoingTransitionsByState = transitions.groupBy { it.start }
 
-    fun partition(start: State): Int = partitionByState[start] ?: throw IllegalStateException("No state with start $start")
+    fun partition(start: State): Int =
+        partitionByState[start] ?: throw IllegalStateException("No state with start $start")
 
-    fun sequence(state: State): Set<Event<T>> = sequenceByState[state] ?: throw IllegalStateException("No sequence for state $state")
+    fun sequence(state: State): Set<Event<T>> =
+        sequenceByState[state] ?: throw IllegalStateException("No sequence for state $state")
 
     private var visitedStates = 0L
 
     fun acceptDepthFirst(visitor: AutomataVisitor<T>) {
         visitedStates = 0
+        visitor.onStart(this)
         visitDepthFirst(visitor, State.Root)
+        visitor.onEnd(this)
     }
 
     fun acceptBreadthFirst(visitor: AutomataVisitor<T>) {
         visitedStates = 0
+        visitor.onStart(this)
         visitBreadthFirst(visitor)
+        visitor.onEnd(this)
     }
 
     private fun visitBreadthFirst(visitor: AutomataVisitor<T>) {
@@ -49,17 +58,17 @@ data class ExtendedPrefixAutomata<T : Comparable<T>>(
             val (state, depth) = deque.removeFirst()
             visitor.onProgress(visitedStates, states.size.toLong())
 
-            visitor.visit(state, depth)
+            visitor.visit(this, state, depth)
 
             val seq = sequence(state)
             val transitions = outgoingTransitionsByState[state] ?: emptyList()
 
             seq.forEach { event ->
-                visitor.visit(event, depth)
+                visitor.visit(this, event, depth)
             }
 
             transitions.forEach { transition ->
-                visitor.visit(transition, depth)
+                visitor.visit(this, transition, depth)
                 deque.addLast(Inner(transition.end, depth + 1))
             }
         }
@@ -70,17 +79,17 @@ data class ExtendedPrefixAutomata<T : Comparable<T>>(
         state: State,
         depth: Int = 0,
     ) {
-        visitor.visit(state, depth)
+        visitor.visit(this, state, depth)
         visitedStates++
         visitor.onProgress(visitedStates, states.size.toLong())
 
         sequence(state).forEach { event ->
-            visitor.visit(event, depth)
+            visitor.visit(this, event, depth)
         }
 
         outgoingTransitionsByState[state]
             ?.onEach { transition ->
-                visitor.visit(transition, depth)
+                visitor.visit(this, transition, depth)
             }?.forEach { transition ->
                 visitDepthFirst(
                     visitor = visitor,
