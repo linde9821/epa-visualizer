f6d473a90a631eb9daf54a63c5283975a6976156
Moritz Lindner <lindner197@gmail.com>
2025-05-11 15:39:07 +0200

    introduce TreeBuildingVisitor to map epa to tree

---
+++ epa/src/main/kotlin/moritz/lindner/masterarbeit/epa/visitor/TreeBuildingVisitor.kt
@@ -0,0 +1,47 @@
+package moritz.lindner.masterarbeit.epa.visitor
+
+import moritz.lindner.masterarbeit.epa.tree.EPATreeNode
+import moritz.lindner.masterarbeit.epa.ExtendedPrefixAutomata
+import moritz.lindner.masterarbeit.epa.domain.State
+import moritz.lindner.masterarbeit.epa.domain.Transition
+
+class TreeBuildingVisitor<T : Comparable<T>> : AutomataVisitor<T> {
+
+    lateinit var root: EPATreeNode<T>
+        private set
+
+    private val stateToNode = mutableMapOf<State, EPATreeNode<T>>()
+    private var currentState: State? = null
+
+    override fun onStart(extendedPrefixAutomata: ExtendedPrefixAutomata<T>) {
+        val rootNode = EPATreeNode<T>(
+            state = State.Root,
+            transitionFromParent = null,
+            parent = null,
+            sequence = extendedPrefixAutomata.sequence(State.Root)
+        )
+        root = rootNode
+        stateToNode[State.Root] = rootNode
+        currentState = State.Root
+    }
+
+    override fun visit(extendedPrefixAutomata: ExtendedPrefixAutomata<T>, state: State, depth: Int) {
+        currentState = state
+    }
+
+    override fun visit(extendedPrefixAutomata: ExtendedPrefixAutomata<T>, transition: Transition, depth: Int) {
+        val parentNode = stateToNode[transition.start]
+            ?: throw IllegalStateException("No node for parent state: ${transition.start}")
+
+        val childNode = EPATreeNode(
+            state = transition.end,
+            transitionFromParent = transition,
+            parent = parentNode,
+            sequence = extendedPrefixAutomata.sequence(transition.end)
+        )
+
+        parentNode.children.add(childNode)
+        stateToNode[transition.end] = childNode
+    }
+
+}
