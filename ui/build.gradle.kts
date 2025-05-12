763439c9f5e205caf96c34627b44c3e7c84649f5
Moritz Lindner <lindner197@gmail.com>
2025-05-12 16:38:29 +0200

    rename main

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "EPAVisualizerKt"
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
