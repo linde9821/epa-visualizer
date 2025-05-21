b08c15ee20358046e72879a25b8f5887dab675de
Moritz Lindner <lindner197@gmail.com>
2025-05-21 21:26:14 +0200

    add thread factory

--- ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/EPAVisualizerMain.kt
+++ ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/EPAVisualizerMain.kt
@@ -1,16 +1,26 @@
 package moritz.lindner.masterarbeit.ui
 
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.asCoroutineDispatcher
 import moritz.lindner.masterarbeit.ui.components.EPAVisualizerUi
 import org.jetbrains.skiko.SkikoProperties
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
 
 val logger = KotlinLogging.logger {}
 
-fun main() =
+fun main() {
+    val threadFactory =
+        ThreadFactory { runnable ->
+            Thread(runnable, "EPA-Visualizer-Background-Thread").apply {}
+        }
+    val executor = Executors.newSingleThreadExecutor(threadFactory)
+    val backgroundDispatcher = executor.asCoroutineDispatcher()
     application {
         Window(
             onCloseRequest = ::exitApplication,
@@ -20,8 +30,11 @@ fun main() =
                     isMinimized = false,
                 ),
             title = "EPA Visualizer",
+            icon = painterResource("logo.png"),
         ) {
             logger.info { "Skiko rendering API: ${SkikoProperties.renderApi.name}" }
-            EPAVisualizerUi()
+            EPAVisualizerUi(backgroundDispatcher)
         }
     }
+    executor.shutdownNow()
+}
