c9fbc128645ca7cb18121903d23dbe7b0f5b8682
Moritz Lindner <lindner197@gmail.com>
2025-09-15 15:36:59 +0200

    move function

--- ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/EPAVisualizerMain.kt
+++ ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/EPAVisualizerMain.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.window.application
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.asCoroutineDispatcher
 import moritz.lindner.masterarbeit.buildconfig.BuildConfig
+import moritz.lindner.masterarbeit.ui.common.AboutPanel.showAboutDialog
 import moritz.lindner.masterarbeit.ui.common.Constants.APPLICATION_NAME
 import moritz.lindner.masterarbeit.ui.common.Icons
 import moritz.lindner.masterarbeit.ui.components.EPAVisualizerUi
@@ -138,38 +139,4 @@ fun main() {
         }
     }
     executor.shutdownNow()
-}
-
-private fun showAboutDialog() {
-    showMessageDialog(
-        null,
-        """
-        $APPLICATION_NAME
-        Version: ${BuildConfig.APP_VERSION}
-        
-        Interactive Visualization of Extended Prefix Automaton Using Radial Trees
-        
-        A tool for analyzing trace variants in large, complex event logs using 
-        Extended Prefix Automata (EPA) and radial tidy tree layouts. This 
-        visualization approach encodes thousands of trace variants while 
-        minimizing visual clutter and supporting interactive filtering.
-        
-        Built as part of a Master's Thesis and Study Project (Studienarbeit) at 
-        Humboldt-Universität zu Berlin.
-        
-        This application and the master thesis is currently under development and not finished.
-        
-        Built with Kotlin and Compose Desktop
-        Process Mining • Event Log Visualization • Variant Analysis
-        
-        Author: Moritz Lindner
-        Supervisor: Prof. Dr. Jan Mendling
-        
-        GitHub: https://github.com/linde9821/epa-visualizer
-        
-        © 2025 Moritz Lindner
-        """.trimIndent(),
-        "About $APPLICATION_NAME",
-        JOptionPane.INFORMATION_MESSAGE
-    )
 }
