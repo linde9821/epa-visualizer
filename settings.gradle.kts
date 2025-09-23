0a4e2edcb42c389d1b55a4763df355f6fed57539
Moritz Lindner <lindner197@gmail.com>
2025-09-23 17:14:16 +0200

    add foojay resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,9 @@ pluginManagement {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-    plugins {}
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    }
 }
 
 rootProject.name = "epa-visualizer"
