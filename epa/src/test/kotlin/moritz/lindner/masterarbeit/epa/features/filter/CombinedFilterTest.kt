42690454e19f7dea548af0d674a52cdc0fd84a1c
Moritz Lindner <lindner197@gmail.com>
2025-09-25 13:50:41 +0200

    add test to ensure multi filter combination works for chaining

---
+++ epa/src/test/kotlin/moritz/lindner/masterarbeit/epa/features/filter/CombinedFilterTest.kt
@@ -0,0 +1,30 @@
+package moritz.lindner.masterarbeit.epa.features.filter
+
+import moritz.lindner.masterarbeit.epa.api.EpaService
+import moritz.lindner.masterarbeit.epa.construction.builder.EpaFromXesBuilder
+import moritz.lindner.masterarbeit.epa.construction.builder.SampleEventMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class CombinedFilterTest {
+
+    @Test
+    fun `applying partition filter and then compression works`() {
+        val epa =
+            EpaFromXesBuilder<Long>()
+                .setFile(File("./src/test/resources/simple2.xes"))
+                .setEventLogMapper(SampleEventMapper())
+                .build()
+        val epaService = EpaService<Long>()
+
+        val partitionFrequencyFilter = PartitionFrequencyFilter<Long>(threshold = 0.5f)
+        val compressionFilter = CompressionFilter<Long>()
+        val filters = listOf(partitionFrequencyFilter, compressionFilter)
+
+        val onlyPartition = partitionFrequencyFilter.apply(epa)
+        val combined = epaService.applyFilters(epa, filters)
+
+        assertThat(combined.getAllPartitions().size).isEqualTo(onlyPartition.getAllPartitions().size)
+    }
+}
