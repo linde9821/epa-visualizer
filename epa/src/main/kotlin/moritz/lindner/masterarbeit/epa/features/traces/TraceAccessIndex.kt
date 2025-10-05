c38257bd1400c9eab3e11df966fd8fe83fd7e666
Moritz Lindner <lindner197@gmail.com>
2025-10-05 21:04:47 +0200

    implement cycle time calculation

--- epa/src/main/kotlin/moritz/lindner/masterarbeit/epa/features/traces/TraceAccessIndex.kt
+++ epa/src/main/kotlin/moritz/lindner/masterarbeit/epa/features/traces/TraceAccessIndex.kt
@@ -2,25 +2,50 @@ package moritz.lindner.masterarbeit.epa.features.traces
 
 import moritz.lindner.masterarbeit.epa.ExtendedPrefixAutomaton
 import moritz.lindner.masterarbeit.epa.domain.Event
+import moritz.lindner.masterarbeit.epa.domain.State
 import moritz.lindner.masterarbeit.epa.visitor.AutomatonVisitor
 
-class TraceAccessIndex<T : Comparable<T>> : AutomatonVisitor<T> {
+class TraceAccessIndex<T> : AutomatonVisitor<T>
+        where T : Comparable<T> {
 
     private val allEvents = mutableListOf<Event<T>>()
 
-    private lateinit var eventsByCaseId: Map<String, List<Event<T>>>
-    private lateinit var caseIdByEvent: Map<Event<T>, String>
+    private lateinit var traceByCaseId: Map<String, List<Event<T>>>
 
     fun getEventsByCase(): Map<String, List<Event<T>>> {
-        return eventsByCaseId
+        return traceByCaseId
+    }
+
+    // can handle compressed
+    fun getCycleTimeOfState(
+        currentEvent: Event<T>,
+        sequence: Set<Event<T>>,
+        state: State
+    ): T? {
+        val trace = traceByCaseId[currentEvent.caseIdentifier]!!
+
+        val traceAtState = trace
+            .dropWhile { event -> !(sequence.contains(event)) }
+            .dropLastWhile { event -> !(sequence.contains(event)) }
+
+        if (traceAtState.last().successorIndex == null) {
+            return null
+        } else {
+            val start = traceAtState.first().timestamp
+            val indexOfNext = traceAtState.last().successorIndex!!
+            val next = trace.find { it.predecessorIndex == traceAtState.last().successorIndex!! - 1 }!!
+            val end = next.timestamp
+//            return end - start
+        }
+        TODO()
     }
 
     fun getTraceByEvent(event: Event<T>): List<Event<T>> {
-        return eventsByCaseId[event.caseIdentifier]!!
+        return traceByCaseId[event.caseIdentifier]!!
     }
 
     override fun onEnd(extendedPrefixAutomaton: ExtendedPrefixAutomaton<T>) {
-        eventsByCaseId = allEvents
+        traceByCaseId = allEvents
             .groupBy { it.caseIdentifier }
             .mapValues { (_, events) -> events.sortedBy(Event<T>::timestamp) }
     }
---
+++ epa/src/test/kotlin/moritz/lindner/masterarbeit/epa/features/cycletime/CycleTimeTest.kt
@@ -0,0 +1,78 @@
+package moritz.lindner.masterarbeit.epa.features.cycletime
+
+import moritz.lindner.masterarbeit.epa.construction.builder.xes.EpaFromXesBuilder
+import moritz.lindner.masterarbeit.epa.construction.builder.xes.SampleEventMapper
+import moritz.lindner.masterarbeit.epa.domain.State
+import moritz.lindner.masterarbeit.epa.features.filter.CompressionFilter
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class CycleTimeTest {
+    @Test
+    fun `must return next event in trace`() {
+        val epa =
+            EpaFromXesBuilder<Long>()
+                .setFile(File("./src/test/resources/sample.xes"))
+                .setEventLogMapper(SampleEventMapper())
+                .build()
+
+        val sut = CycleTime<Long>()
+
+        epa.acceptDepthFirst(sut)
+
+        val state = epa.states.first { it.name == "b" && (it as? State.PrefixState)?.from?.name == "a" }
+
+        val event = epa.sequence(state).first()
+
+        val next = sut.getNextEventInTraceAtDifferentState(event, state)
+
+        assertThat(next.predecessorIndex).isEqualTo(event.successorIndex!! - 1)
+        assertThat(next.caseIdentifier).isEqualTo(event.caseIdentifier)
+        assertThat(next.activity.name).isEqualTo("c")
+    }
+
+    @Test
+    fun `must return next event of other state when epa is compressed`() {
+        val epa1 =
+            EpaFromXesBuilder<Long>()
+                .setFile(File("./src/test/resources/sample.xes"))
+                .setEventLogMapper(SampleEventMapper())
+                .build()
+
+        val filter = CompressionFilter<Long>()
+        val epa = filter.apply(epa1)
+
+        val sut = CycleTime<Long>()
+
+        epa.acceptDepthFirst(sut)
+
+        val state = epa.states.first { it.name == "b-c-d" }
+
+        val event = epa.sequence(state).first()
+
+        val next = sut.getNextEventInTraceAtDifferentState(event, state)
+
+        assertThat(next.activity.name).isEqualTo("f")
+    }
+
+    @Test
+    fun `cycle time must be correct value`() {
+        val epa =
+            EpaFromXesBuilder<Long>()
+                .setFile(File("./src/test/resources/sample.xes"))
+                .setEventLogMapper(SampleEventMapper())
+                .build()
+
+        val sut = CycleTime<Long>()
+
+        epa.acceptDepthFirst(sut)
+
+        val state = epa.states.first { it.name == "b" && (it as? State.PrefixState)?.from?.name == "a" }
+        val event = epa.sequence(state).first()
+
+        val ct = sut.cycleTimeOfEventInTrace(event, state, Long::minus)
+
+        assertThat(ct).isEqualTo(1L * 60 * 1000)
+    }
+}
