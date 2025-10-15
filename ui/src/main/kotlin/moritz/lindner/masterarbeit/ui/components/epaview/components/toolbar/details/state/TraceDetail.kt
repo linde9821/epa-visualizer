6a122caed20c9102649f902b342641bc63352391
Moritz Lindner <lindner197@gmail.com>
2025-10-15 10:51:56 +0200

    improve trace event view

--- ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/components/epaview/components/toolbar/details/state/TraceDetail.kt
+++ ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/components/epaview/components/toolbar/details/state/TraceDetail.kt
@@ -1,16 +1,21 @@
 package moritz.lindner.masterarbeit.ui.components.epaview.components.toolbar.details.state
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -18,6 +23,7 @@ import moritz.lindner.masterarbeit.epa.ExtendedPrefixAutomaton
 import moritz.lindner.masterarbeit.epa.api.EpaService
 import moritz.lindner.masterarbeit.epa.domain.Event
 import moritz.lindner.masterarbeit.epa.domain.State
+import moritz.lindner.masterarbeit.ui.common.Formatting.asFormattedLocalDateTime
 import org.jetbrains.jewel.ui.component.Icon
 import org.jetbrains.jewel.ui.component.IconButton
 import org.jetbrains.jewel.ui.component.Text
@@ -32,16 +38,22 @@ fun TraceDetail(
 ) {
     var show by remember { mutableStateOf(false) }
 
-    Row {
-        Text("Trace ${trace.first().caseIdentifier}")
-        IconButton(
-            onClick = { show = !show },
-            modifier = Modifier.padding(start = 8.dp)
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            if (!show) {
-                Icon(AllIconsKeys.General.ChevronDown, "Chevron")
-            } else {
-                Icon(AllIconsKeys.General.ChevronUp, "Chevron")
+            Text("Trace: ${trace.first().caseIdentifier}")
+            IconButton(
+                onClick = { show = !show },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                if (!show) {
+                    Icon(AllIconsKeys.General.ChevronDown, "Chevron")
+                } else {
+                    Icon(AllIconsKeys.General.ChevronUp, "Chevron")
+                }
             }
         }
 
@@ -56,15 +68,17 @@ fun TraceDetail(
                     } else {
                         FontWeight.Normal
                     }
-                    Text(
-                        "${index + 1}: ${event.activity} at ${event.timestamp} for ${stateOfEvent?.name}",
-                        fontWeight = weight,
-                        modifier = Modifier.clickable(
-                            onClick = {
-                                onStateSelected(stateOfEvent!!)
-                            }
+                    SelectionContainer {
+                        Text(
+                            "${index + 1}: ${event.activity} at ${event.timestamp.asFormattedLocalDateTime()}",
+                            fontWeight = weight,
+                            modifier = Modifier.clickable(
+                                onClick = {
+                                    onStateSelected(stateOfEvent!!)
+                                }
+                            )
                         )
-                    )
+                    }
                     Spacer(Modifier.height(1.dp))
                 }
             }
