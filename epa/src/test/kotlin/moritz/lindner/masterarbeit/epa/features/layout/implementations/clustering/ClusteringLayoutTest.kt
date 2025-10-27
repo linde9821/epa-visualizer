fb9b3be75fe01908e9a1876fa9b385e0bad8e8fc
Moritz Lindner <lindner197@gmail.com>
2025-10-27 12:12:13 +0100

    Revert "remove unused test"
    This reverts commit 4d0674fe71a9bc00524efb172fc7f655b85d7af5.

---
+++ epa/src/test/kotlin/moritz/lindner/masterarbeit/epa/features/layout/implementations/clustering/ClusteringLayoutTest.kt
@@ -0,0 +1,36 @@
+package moritz.lindner.masterarbeit.epa.features.layout.implementations.clustering
+
+import moritz.lindner.masterarbeit.epa.construction.builder.xes.EpaFromXesBuilder
+import moritz.lindner.masterarbeit.epa.construction.builder.xes.SampleEventMapper
+import moritz.lindner.masterarbeit.epa.features.layout.factory.LayoutConfig
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class ClusteringLayoutTest {
+    @Test
+    fun `must create a valid layout`() {
+        val epa =
+            EpaFromXesBuilder<Long>()
+                .setFile(File("./src/test/resources/sample.xes"))
+                .setEventLogMapper(SampleEventMapper())
+                .build()
+
+        val clusteringLayout = ClusteringLayout(
+            epa,
+            config = LayoutConfig.ClusteringLayoutConfig(
+                useGraphEmbedding = true,
+                umapK = 5,
+                umapIterations = 200,
+                useForceDirected = false,
+                useResolveOverlap = false,
+            )
+        )
+
+        assertThat(clusteringLayout.isBuilt()).isFalse
+        clusteringLayout.build()
+        assertThat(clusteringLayout.isBuilt()).isTrue
+
+    }
+
+}
