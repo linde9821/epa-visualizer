b3f0f773f64fef7a66a3068519fd844cdc7e877b
Moritz Lindner <lindner197@gmail.com>
2025-12-29 14:04:32 +0100

    add state highlighting

--- ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/components/epaview/components/tree/TreeCanvasRenderingHelper.kt
+++ ui/src/main/kotlin/moritz/lindner/masterarbeit/ui/components/epaview/components/tree/TreeCanvasRenderingHelper.kt
@@ -39,6 +39,8 @@ object TreeCanvasRenderingHelper {
 
             if (highlightingAtlas.pathFromRootStates.contains(state)) {
                 canvas.nativeCanvas.drawCircle(cx, cy, entry.size + 15f, drawAtlas.pathFromRootPaint)
+            }else if (highlightingAtlas.outgoingPathsState.contains(state)) {
+                canvas.nativeCanvas.drawCircle(cx, cy, entry.size + 15f, drawAtlas.outgoingPathsPaint)
             }
 
             canvas.nativeCanvas.drawCircle(cx, cy, entry.size, paint)
