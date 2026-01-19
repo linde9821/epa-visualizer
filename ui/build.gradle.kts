a39ce11145afa1e389a09f56af70869bdcc9ba8e
Moritz Lindner <lindner197@gmail.com>
2026-01-19 10:10:35 +0100

    remove hot reload as its now bundled

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.compose)
     alias(libs.plugins.kotlin.plugin.compose)
     alias(libs.plugins.gradle.buildconfig.plugin)
-    alias(libs.plugins.composeHotReload)
     alias(libs.plugins.kotlin.serialization)
 }
 
