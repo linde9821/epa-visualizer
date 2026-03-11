31c09cb6d2e7cd4108f69648eaa8e8f1acacc1a3
Moritz Lindner <lindner197@gmail.com>
2026-03-11 14:02:21 +0100

    add sepsis event log

--- epa/src/main/kotlin/moritz/lindner/masterarbeit/metrics/filter/CompleteFilterEvaluation.kt
+++ epa/src/main/kotlin/moritz/lindner/masterarbeit/metrics/filter/CompleteFilterEvaluation.kt
@@ -39,7 +39,7 @@ fun main() {
     val challenge2017 = File(repoRoot, "/data/eventlogs/BPI Challenge 2017.xes.gz") to BPI2017ChallengeEventMapper()
     val challenge2018 = File(repoRoot, "/data/eventlogs/BPI Challenge 2018.xes.gz") to BPI2018ChallengeMapper()
     val challenge2020Internationale = File(repoRoot, "/data/eventlogs/InternationalDeclarations.xes.gz") to BPI2020()
-    val sepsis = File(repoRoot, "/data/eventlogs/Sepsis Cases - Event Log.xes.gz") to Sepsis()
+    val sepsis = File(repoRoot, "/data/eventlogs/Sepsis Cases - Event Log.xes") to Sepsis()
 
     val logs = listOf(
         challenge2017Offer2017,
